1e48014c92fb9602fd65dce16939a230922a99b9
Коробова Марина Алексеевна <104667248+stupid-penguin@users.noreply.github.com>
2022-10-10 19:40:49 +0500

    Add files via upload

---
+++ data-analysis/classwork1/data_types.R
@@ -0,0 +1,25 @@
+# Присвойте значение 33.3 переменной my_numeric
+my_numeric <- 33.3
+
+# Проверьте тип данных переменной
+class(my_numeric)
+
+# Смените значение my_numeric на 42
+my_numeric <- 42
+
+# Проверьте тип данных my_numeric
+class(my_numeric)
+
+# Установите значение my_character в "МИР"
+my_character <- "Привет"
+my_character <- "МИР"
+
+# Проверьте тип данных my_numeric
+class(my_character)
+
+# Установите my_logical в FALSE
+my_logical <- TRUE
+my_logical <- FALSE
+
+# Проверьте тип данных my_numeric
+class(my_logical)
